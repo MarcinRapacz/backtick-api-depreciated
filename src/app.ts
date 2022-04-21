8861b8e24642ff3a1623f3e6437c0bbdf2687e89
MarcinRapacz <marcin_04@onet.eu>
2022-04-21 20:48:08 +0200

    feat: add body parser

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler } from './utils/errorHandler';
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
 app.use('/api', router);
 app.use(errorHandler);
 
